bfec45c10d08c44525d40f93f58fe6a8c8df0298
Rob Whitaker <robjameswhitaker@gmail.com>
2019-12-09 23:44:03 -0500

    Switched to a mutable Array for massive efficiency improvements.

--- 2019/day9/solution.hs
+++ 2019/day9/solution.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-import Data.Array
+import Data.Array.IO
 import Control.Monad (void)
 import System.IO (hFlush, hPutStr, stdout, stderr)
 
-import Debug.Trace
-
-type Memory = Array Int Int
+type Memory = IOUArray Int Int
 
 data Program = Program
     { progRelativeBase :: Int
     , progMemory :: Memory
-    } deriving (Show)
+    }
 
 data Pointer = Ref Int | Rel Int
   deriving (Show)
@@ -32,71 +30,91 @@ data Op
     | Halt
   deriving (Show)
 
-parseOpAt :: Int -> Memory -> Op
-parseOpAt ix mem =
+parseOpAt :: Int -> Memory -> IO Op
+parseOpAt ix mem = do
+    opCode <- readArray mem ix
+    let intOp = opCode `mod` 100
+        paramModeAt n = opCode `div` (10 ^ (n+1)) `mod` 10
+
+        param :: Int -> IO Param
+        param n =
+            let p = case paramModeAt n of
+                        0 -> Mem . Ref
+                        1 -> Val
+                        2 -> Mem . Rel
+            in do
+            val <- readArray mem (ix + n)
+            return $ p val
+
+        pointerParam :: Int -> IO Pointer
+        pointerParam n =
+            let p = case paramModeAt n of
+                    0 -> Ref
+                    2 -> Rel
+            in do
+            val <- readArray mem (ix + n)
+            return $ p val
+
     case intOp of
-        1 -> Add (param 1) (param 2) (pointerParam 3)
-        2 -> Mul (param 1) (param 2) (pointerParam 3)
-        3 -> In (pointerParam 1)
-        4 -> Out (pointerParam 1)
-        5 -> JumpIfTrue (param 1) (param 2)
-        6 -> JumpIfFalse (param 1) (param 2)
-        7 -> LessThan (param 1) (param 2) (pointerParam 3)
-        8 -> Equals (param 1) (param 2) (pointerParam 3)
-        9 -> AdjustRelativeBase (param 1)
-        99 -> Halt
+        1 -> Add <$> (param 1)
+                 <*> (param 2)
+                 <*> (pointerParam 3)
+        2 -> Mul <$> (param 1)
+                 <*> (param 2)
+                 <*> (pointerParam 3)
+        3 -> In  <$> (pointerParam 1)
+        4 -> Out <$> (pointerParam 1)
+        5 -> JumpIfTrue  <$> (param 1)
+                         <*> (param 2)
+        6 -> JumpIfFalse <$> (param 1)
+                         <*> (param 2)
+        7 -> LessThan <$> (param 1)
+                      <*> (param 2)
+                      <*> (pointerParam 3)
+        8 -> Equals   <$> (param 1)
+                      <*> (param 2)
+                      <*> (pointerParam 3)
+        9 -> AdjustRelativeBase <$> (param 1)
+        99 -> return Halt
         _ -> error $ "Invalid op code: " <> show intOp
-  where
-    opCode = mem ! ix
-    intOp = opCode `mod` 100
-    paramModeAt n = opCode `div` (10 ^ (n+1)) `mod` 10
-    param :: Int -> Param
-    param n =
-        let p = case paramModeAt n of
-                    0 -> Mem . Ref
-                    1 -> Val
-                    2 -> Mem . Rel
-        in
-        p (mem ! (ix + n))
-    pointerParam :: Int -> Pointer
-    pointerParam n =
-        let p = case paramModeAt n of
-                0 -> Ref
-                2 -> Rel
-        in
-        p (mem ! (ix + n))
 
-getValue :: Param -> Program -> Int
-getValue (Val n) _ = n
-getValue (Mem (Ref ix)) (Program { progMemory }) = progMemory ! ix
+getValue :: Param -> Program -> IO Int
+getValue (Val n) _ = return n
+getValue (Mem (Ref ix)) (Program { progMemory }) = readArray progMemory ix
 getValue (Mem (Rel ix)) (Program { progRelativeBase, progMemory }) =
-    progMemory ! (ix + progRelativeBase)
+    readArray progMemory (ix + progRelativeBase)
 
-setValue :: Int -> Pointer -> Program -> Program
+setValue :: Int -> Pointer -> Program -> IO ()
 setValue val pointer prog@(Program {progRelativeBase, progMemory }) =
     let ix = case pointer of
             Ref addr -> addr
             Rel offset -> progRelativeBase + offset
-    in
-    prog { progMemory = progMemory // [(ix, val)] }
+    in do
+    writeArray progMemory ix val
 
 eval :: Int -> Program -> IO Program
-eval ix prog@(Program { progRelativeBase, progMemory }) =
-    case parseOpAt ix progMemory of
-        Add p1 p2 dest ->
-            eval (ix + 4) (setValue (getValue p1 prog + getValue p2 prog) dest prog)
+eval ix prog@(Program { progRelativeBase, progMemory }) = do
+    opt <- parseOpAt ix progMemory
+    case opt of
+        Add p1 p2 dest -> do
+            newVal <- (+) <$> getValue p1 prog <*> getValue p2 prog
+            setValue newVal dest prog
+            eval (ix + 4) prog
 
-        Mul p1 p2 dest ->
-            eval (ix + 4) (setValue (getValue p1 prog * getValue p2 prog) dest prog)
+        Mul p1 p2 dest -> do
+            newVal <- (*) <$> getValue p1 prog <*> getValue p2 prog
+            setValue newVal dest prog
+            eval (ix + 4) prog
 
         In dest -> do
             hPutStr stderr "Input: "
             hFlush stderr
             inNum <- read <$> getLine
-            eval (ix + 2) (setValue inNum dest prog)
+            setValue inNum dest prog
+            eval (ix + 2) prog
 
         Out pointer -> do
-            putStrLn . show $ getValue (Mem pointer) prog
+            getValue (Mem pointer) prog >>= putStrLn . show
             hFlush stdout
             eval (ix + 2) prog
 
@@ -112,33 +130,40 @@ eval ix prog@(Program { progRelativeBase, progMemory }) =
         Equals p1 p2 dest ->
             compareParams (==) p1 p2 dest
 
-        AdjustRelativeBase p1 ->
+        AdjustRelativeBase p1 -> do
+            offset <- getValue p1 prog
             eval
                 (ix + 2)
-                (prog { progRelativeBase = progRelativeBase + getValue p1 prog })
+                (prog { progRelativeBase = progRelativeBase + offset })
 
         Halt ->
             return prog
   where
-    jumpIf pred p1 p2 =
-        if pred (getValue p1 prog)
-            then eval (getValue p2 prog) prog
+    jumpIf pred p1 p2 = do
+        p1Val <- getValue p1 prog
+        p2Val <- getValue p2 prog
+        if pred p1Val
+            then eval p2Val prog
             else eval (ix + 3) prog
 
-    compareParams pred p1 p2 dest =
-        if pred (getValue p1 prog) (getValue p2 prog)
-            then eval (ix + 4) (setValue 1 dest prog)
-            else eval (ix + 4) (setValue 0 dest prog)
+    compareParams pred p1 p2 dest = do
+        p1Val <- getValue p1 prog
+        p2Val <- getValue p2 prog
+        if pred p1Val p2Val
+            then do
+                setValue 1 dest prog
+                eval (ix + 4) prog
+            else do
+                setValue 0 dest prog
+                eval (ix + 4) prog
 
 getInput :: IO Program
 getInput = do
     input <- readFile "input.txt"
     let inputAsList = read ("[" <> input <> "]") <> take 5000 [0,0..]
-    return $ Program 0 (listArray (0, length inputAsList - 1) inputAsList)
+    arr <- newListArray (0, length inputAsList - 1) inputAsList
+    return $ Program 0 arr
 
--- Answers both problem 1 & 2 since the answers only vary by runtime input
--- Input for problem 1: 1
--- Input for problem 2: 5
 main :: IO ()
 main = do
     input <- getInput
